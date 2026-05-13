6cc6efaf662cb0efb5ca00bd9644cef5bc7657c1
unknown <vidit.joshi2102@gmail.com>
2026-05-13 14:48:47 +0530

    add 13/05

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,13 @@
 const cloudinary = require('cloudinary').v2;
 
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY || process.env.API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET || process.env.API_SECRET;
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
 });
 
 module.exports = cloudinary;
